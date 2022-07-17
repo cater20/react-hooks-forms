3b52208004412df257cdea17b015d1a26ec34559
chrisphine <onyangoketta@gmail.com>
2022-07-17 15:30:49 +0300

    created parent component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+const [newsLetter,setNewsLetter]=useState(false)
 
 function handleFirstNameChange(event){
   setFirstName(event,target,value)
@@ -10,11 +11,18 @@ function handleFirstNameChange(event){
 function handleLastNameChange(event){
   setLastName(event,target,value)
 }
+function handleNewsLetterChange(event){
+  setNewsLetter(event,target,value)
+}
 
   return (
     <form>
+      <label htmlFor="newsLetter">Subscribe to our Newsletter?</label>
       <input type="text" onChange={handleFirstNameChange}value={firstName} />
       <input type="text" onChange={handleLastNameChange}value={lastName} />
+      <input type={checkedbox} id="newsLetter" onChange={handleNewsletterChange}
+      
+        checked={newsLetter} />
       <button type="submit">Submit</button>
     </form>
   );
---
+++ src/components/Parent.js
@@ -0,0 +1,4 @@
+import React,{useState} from "react";
+import Form from "./Form";
+
+
